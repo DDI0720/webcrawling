d76f2f81a172ca60d4c0bdae097c1334ee9b6aa6
DDI0720 <rlaskdn3615@gmail.com>
2020-06-01 21:55:47 +0900

    폴바셋메뉴를 가져오는 작업까지

---
+++ index.js
@@ -0,0 +1,16 @@
+
+//모듈 추출
+const request = require('request');
+const cheerio = require('cheerio'); //request 모듈 사용
+const url = 'https://www.baristapaulbassett.co.kr/menu/List.pb?cid1=A';
+
+request(url, function(err, response, body) {
+    if (err) throw error;
+    const $ = cheerio.load(body);
+    let data = $('div.menuList ul.listStyleB');
+    let arr = [];
+    data.each(function(i,elem){
+        arr[i] = $(this).find("a").children("div.txtArea").text();
+    });
+    console.log(arr)
+});
