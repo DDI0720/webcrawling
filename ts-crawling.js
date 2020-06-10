a6bff7417ce8236397468a630e4b8c1b98039f51
DDI0720 <rlaskdn3615@gmail.com>
2020-06-10 23:49:21 +0900

    1일1커밋

--- ts-crawling.js
+++ ts-crawling.js
@@ -3,18 +3,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var cheerio = require("cheerio");
+var cheerio_1 = __importDefault(require("cheerio"));
 var request_1 = __importDefault(require("request"));
-var arr = [], arr2 = [];
 var url = 'https://www.baristapaulbassett.co.kr/menu/List.pb?cid1=A';
 request_1.default(url, function (err, response, body) {
     if (err)
         throw err;
-    var $ = cheerio.load(body);
-    var data = $('div.menuList ul.listStyleB li');
-    data.each(function (i, elem) {
-        arr[i] = cheerio(elem).find("a").children("div.txtArea").text();
-    });
-    response.read();
+    console.log('11111::', body);
+    var $ = cheerio_1.default.load(body);
+    console.log('22222::', $);
 });
-//# sourceMappingURL=ts-crawling.js.map
--- ts-crawling.ts
+++ ts-crawling.ts
@@ -1,5 +1,14 @@
-import cheerio from 'cheerio';
+import cheerio = require('cheerio');
 import request from 'request';
+let arr:string[] = [], arr2: string[] = [];
+const url = 'https://www.baristapaulbassett.co.kr/menu/List.pb?cid1=A';
 
-console.log(cheerio)
-console.log(request)
+request(url, function(err, response, body){
+    if(err) throw err;
+    const $ = cheerio.load(body);
+    let data = $('div.menuList ul.listStyleB li');
+    data.each(function(i,elem){
+        arr[i] = cheerio(elem).find("a").children("div.txtArea").text();
+    });
+})
+//ㅋㅓ밋용 ㅋㅋㅋ
